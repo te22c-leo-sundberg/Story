cb045edfb39d3ba9f58b5fb771631b3da6a98487
Leo Sundberg <leo.sundberg@elev.ga.ntig.se>
2023-09-04 09:32:28 +0200

    endoflessoncommit

--- Story/Program.cs
+++ Story/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("!");
+﻿
+Console.WriteLine("What is your name?");
+string name = Console.ReadLine().ToLower();
+
+if (name == "leo")
+{
+Console.WriteLine("Dangerous Individual Detected. Immediate Execution Initialized");
+}
+else if (name == "astolfo")
+{
+    Console.WriteLine("Femboy Detected. Initialize Milking Chamber");
+}
+else
+{
+    Console.WriteLine("Welcome, Upstanding Citizen");
+}
+
+// {
+//     Console.WriteLine("Welcome, Upstanding Citizen");
+// }
+
+Console.ReadLine();
